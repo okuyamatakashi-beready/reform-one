385d485ab74e8117ae5220a1e600e3bb4b47f1f6
奥山貴氏 <>
2024-08-05 14:55:57 +0900

    first チューニング

--- header.php
+++ header.php
@@ -15,44 +15,42 @@
 		<link href="<?php echo get_template_directory_uri(); ?>/scss/slick.css" rel="stylesheet" type="text/css">
 		<link rel="icon" href="/images/common/favicon.ico" id="favicon">
 
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
 		
 			<?php wp_head();?>
 	</head>
 <body>
-
 <header id="header">
-	<div class="header__wrap">
+	<div class="header__container">
 		<div class="flex">
-			<nav id="gnav">
-				<ul class="flex">
-					<li>
-						<a href="">郡上ODORIについて</a>
-					</li>
-					<li>
-						<a href="">郡上ODORIへの想い</a>
-					</li>
-					<li class="has-submenu">
-						<a href="" class="flex">過去の開催<img src="<?php echo get_template_directory_uri(); ?>/images/nav-arrow.svg" alt=""></a>
-						<ul class="hidden">
-							<li>
-								<a href="">・2023</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
-
-			<a href="" class="header__contact">
-				<div class="flex">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/mail-icon.svg" alt="">
-					<strong>CONTACT</strong>
-				</div>
-				<p>お問い合わせ</p>
+			<a href="/" class="header__logo">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/common/logo.svg" alt="">
 			</a>
+
+			<div class="header__right">
+				<div class="flex">
+					<a href="/contact" class="header__contact">
+						<span>contact</span>
+					</a>
+					<div id="toggle">
+						<div>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 		</div>
+
 	</div>
+
 </header>
+
+
 
 	
 
